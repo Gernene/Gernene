3260c60ba2fe35e6975a54493ffe6fd222240d3a
Gernene <wallie988@gmail.com>
2018-01-06 13:36:14 -0700

    Add website gallery

--- js/main.js
+++ js/main.js
@@ -5,14 +5,6 @@ $(document).ready(function() {
     $('#nav nav').slideToggle();
   }
 
-  $('.gallery-item').on('click', function() {
-    var id = $(this).attr("id").replace("gallery-item-","");
-    var galleryView = "#gallery-viewer-item-" + id;
-    $('.gallery-viewer-item').hide();
-    $('.gallery-viewer').show();
-    $(galleryView).show();
-  });
-
   $('.gallery-cross i').on('click', function() {
     $('.gallery-viewer').hide();
   });
